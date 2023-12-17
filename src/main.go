74b52edde7ad0a92fb857c9b6f973ed8ae5cc8b8
Bruno Souto <bcovies@gmail.com>
2023-12-17 19:40:48 -0300

    add basic web example

---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rhattox/go-rest-api-example/handlers"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", handlers.PingHandler)
+	
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
